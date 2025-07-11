d947307a30acf624865d3bcb5b815edfb7a0c318
yujin-xin <barete.mart@gmail.com>
2025-07-11 15:38:33 +0800

    init

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-const port = 8080;
+const port = process.env.PORT || 3000;
 
 // ✅ Serve static files from /public
 app.use(express.static(path.join(__dirname, 'public')));
